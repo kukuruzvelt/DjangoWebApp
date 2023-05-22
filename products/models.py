3a62c41ff3b8972d4ac4653ae013b3b1b95e36a8
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-05-22 15:49:08 +0300

    1

--- products/models.py
+++ products/models.py
@@ -9,20 +9,24 @@ class Product(models.Model):
     description = models.TextField('Description')
 
     def __str__(self):
-        return self.name + "\n\n" + self.description
+        return f"{self.name} - {self.description} : {self.quantity} left"
 
 
 class Cart(models.Model):
-    user = models.ManyToManyField(Customer)
-    product = models.ManyToManyField(Product)
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=None)
     quantity = models.BigIntegerField('Quantity', default=1)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.product.price} x {self.quantity} = {self.product.price * self.quantity}"
 
 
 class Order(models.Model):
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
     date = models.DateField('Delivery date')
     city = models.CharField('City', max_length=30, default='')
 
 
 class OrderProduct(models.Model):
-    order = models.ManyToManyField(Order)
-    product = models.ManyToManyField(Product)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, default=None)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=None)
--- products/views.py
+++ products/views.py
@@ -27,12 +27,10 @@ def catalog(request):
                 data['error_message'] = 'This product is out of stock'
         else:
             data['error_message'] = 'Please log in before adding products to cart'
-    # todo add link to catalog on main page
 
-    # showing only products with quantity > 0
+    # only showing products with quantity > 0
     products = Product.objects.filter(quantity__gt=0)
     data['products'] = products
-    # todo show how many products left on catalog page
     return render(request, 'products/catalog.html', data)
 
 
@@ -41,11 +39,12 @@ def catalog(request):
 def cart(request):
     data = {}
 
+    user = Customer.objects.get(user=request.user)
+
     if request.method == 'POST':
         # removing item from cart
         product_id = request.POST['prod_id']
         product = Product.objects.get(id=product_id)
-        user = Customer.objects.get(user=request.user)
 
         Product.objects.filter(id=product_id).update(quantity=product.quantity + 1)
 
@@ -54,13 +53,12 @@ def cart(request):
             Cart.objects.filter(user=user, product=product).update(quantity=cart_instance.quantity - 1)
         else:
             Cart.objects.filter(user=user, product=product).delete()
-    products = get_products_for_user(user)
-    if not products:
-        data['message'] = 'Your cart is empty'
+    cart_list = Cart.objects.filter(user=user)
+    if cart_list.__len__() > 0:
+        data['cart'] = cart_list
     else:
-        data['products'] = products
+        data['message'] = 'Your cart is empty'
 
-    # todo return with quantity of products in cart
     return render(request, 'products/cart.html', data)
 
 
@@ -74,13 +72,15 @@ def buy(request):
     products = get_products_for_user(user)
 
     if request.method == 'POST':
-        # todo consider adding user to order
         form = OrderForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = Order.objects.create(user=user, date=form.cleaned_data.get('date'),
+                                         city=form.cleaned_data.get('city'))
+            order.save()
             for prod in products:
                 OrderProduct.objects.create(order=order, product=prod).save()
                 Cart.objects.filter(user=user, product=prod).delete()
+                # todo redirect to profile with message about successful order creation
         else:
             data['error_message'] = 'Form was wrong, try again'
 
@@ -92,7 +92,6 @@ def buy(request):
 
 
 def get_products_for_user(user):
-    # todo try to get all products without cycle
     products = []
     items = Cart.objects.filter(user=user)
     for item in items:
