8b84aa6f8daab70ac6b53390963fafed73084e34
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-05-28 16:09:10 +0300

    csrf trusted origins update2

--- djangoWeb/settings.py
+++ djangoWeb/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
